51783736927e3db18fd872862da9570bc1804341
artF412 <Chalantorn.sup@gmail.com>
2023-03-10 14:51:24 +0700

    finish

--- fish_buzz.py
+++ fish_buzz.py
@@ -1,10 +1,26 @@
-try:
-    number = int(input('Enter Number : '))
-    for i in range (number):
-        if ((i+1) % 5) == 0:
-            print('Buzz')
+while True:
+    try:
+        round = int(input('Enter Round : '))
+        if round > 0:
+            break
         else:
-            print('Fit')
-except ValueError:
-    print('Error Sorry pls Enter Number!!')
+            raise ValueError
+    except ValueError:
+        print('Error Sorry Enter Round Number again pls!!')
+        
+while True: 
+    try:
+        point = int(input('Enter Point : '))
+        if point > 0:
+            break
+        else:
+            raise ValueError
+    except ValueError:
+        print('Error Sorry pls Enter Point Number again pls!!')
+
+for i in range (round):
+    if ((i+1) % point) == 0:
+        print('Buzz')
+    else:
+        print('Fit')
         
